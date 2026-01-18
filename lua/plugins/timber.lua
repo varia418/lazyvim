009b6bdcd2daf3220f219aec0cb5cf4134f09e73
varia <ltnhan2610@gmail.com>
2026-01-18 16:35:43 +0700

    Replace debugprint with timber for logging

---
+++ lua/plugins/timber.lua
@@ -0,0 +1,23 @@
+return {
+  "Goose97/timber.nvim",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  opts = {
+    log_templates = {
+      default = {
+        lua = [[print("%log_marker %log_target", %log_target)]],
+        javascript = [[console.log("%log_marker ~ %log_target:", %log_target)]],
+        typescript = [[console.log("%log_marker ~ %log_target:", %log_target)]],
+        jsx = [[console.log("%log_marker ~ %log_target:", %log_target)]],
+        tsx = [[console.log("%log_marker ~ %log_target:", %log_target)]],
+      },
+      plain = {
+        javascript = [[console.log("%log_marker ~", %insert_cursor)]],
+        typescript = [[console.log("%log_marker ~", %insert_cursor)]],
+        jsx = [[console.log("%log_marker ~", %insert_cursor)]],
+        tsx = [[console.log("%log_marker ~", %insert_cursor)]],
+      },
+    },
+    log_marker = "🪵", -- Or any other string, e.g: MY_LOG
+  },
+}
