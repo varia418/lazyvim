add27eb4eb394e5324e8936bcbce8ccc98790e44
nhan.lethanh <nhan.lethanh@tpssoft.com>
2026-04-12 23:41:02 +0700

    Remap navigational keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
+-- jkl; navigation
+vim.keymap.set({ "n", "v", "x", "o" }, "j", "<Left>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v", "x", "o" }, "k", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim.keymap.set({ "n", "v", "x", "o" }, "l", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.keymap.set({ "n", "v", "x", "o" }, ";", "<Right>", { noremap = true, silent = true })
 
-map("n", "j", "h")
-map("n", "k", "j")
-map("n", "l", "k")
-map("n", ";", "l")
+-- Window navigation
+vim.keymap.set("n", "<C-w>j", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-w>k", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-w>l", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-w>;", "<C-w>l", { noremap = true, silent = true })
 
-map("v", "j", "h")
-map("v", "k", "j")
-map("v", "l", "k")
-map("v", ";", "l")
+-- Remap ; (find next char for f/t) to '
+vim.keymap.set({ "n", "v", "x", "o" }, "'", ";", { noremap = true, silent = true })
