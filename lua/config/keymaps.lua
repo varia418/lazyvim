0d389fc7cc49e76b02386edfeb09a23115637974
nhan.lethanh <nhan.lethanh@tpssoft.com>
2026-04-12 22:42:03 +0700

    Remap navigational keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "j", "h")
+map("n", "k", "j")
+map("n", "l", "k")
+map("n", ";", "l")
+
+map("v", "j", "h")
+map("v", "k", "j")
+map("v", "l", "k")
+map("v", ";", "l")
